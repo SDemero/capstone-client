ba30ff2c16570a350ae56de14ba503a24f316a1c
Tyler Thomas <tylerthomas327@gmail.com>
2021-01-26 11:49:47 -0500

    Home page in a good state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home.jsx'
+import './Components/Home.css'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     
+     <Home/>
     </div>
+    </Router>
   );
 }
 
